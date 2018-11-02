53584cb76ff58c2aae4373dd578d98cfbd04f6e5
Hailey3338 <Hailey3338@hotmail.com>
2018-11-02 13:22:22 +0100

    Create IWorkerRepository.cs

---
+++ IWorkerRepository.cs
@@ -0,0 +1,14 @@
+using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace EntityFrameworkWeb
+{
+	public interface IWorkerRepository<Worker>
+	{
+		void Insert(Worker worker);
+		void Delete(Worker worker);
+		IQueryable<Worker> SearchFor(Express<Func<worker.bool>>predicate);
+		IQueryable<Worker> GetAll();
+		Worker GetById(int WorkerID);
+	}
+}
