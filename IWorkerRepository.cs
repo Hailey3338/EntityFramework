624aee1ba8e5a15b40885fd5370ef5144b7c6e00
Hailey3338 <Hailey3338@hotmail.com>
2018-11-02 13:32:13 +0100

    Update IWorkerRepository.cs

--- IWorkerRepository.cs
+++ IWorkerRepository.cs
@@ -3,13 +3,12 @@ using System.ComponentModel.DataAnnotations;
 
 namespace EntityFrameworkWeb
 {
-	public interface IWorkerRepository<Worker>
+	public interface IWorkerRepository<Worker> where Worker : class
 	{
+		Worker GetById(int id);
+		IQueryable<Worker> GetAll();
 		void Insert(Worker worker);
 		void Delete(Worker worker);
-		IQueryable<Worker> SearchFor(Express<Func<worker.bool>>predicate);
-		IQueryable<Worker> GetAll();
-		Worker GetById(int WorkerID);
 		void SaveChanges();
 	}
 }
